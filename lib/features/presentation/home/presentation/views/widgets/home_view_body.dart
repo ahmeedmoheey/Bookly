f100f9dbe90aee7f338b4fdd66d4cb80ac33e460
ahmed <ahmedmohie553@gmail.com>
2025-02-25 00:45:22 +0200

    custom list view

--- lib/features/presentation/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/presentation/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/images_manager.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -7,8 +9,16 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [CustomAppBar()],
+      children: const [CustomAppBar(),
+        CustomListViewItem()
+
+      ]
+
     );
   }
+
+
+
 }
+
 
