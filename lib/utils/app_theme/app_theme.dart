463ec8760a699e20cb58e952bf24d89d8e1750c6
ahmed <ahmedmohie553@gmail.com>
2025-02-25 00:17:18 +0200

    +custom appBar tom home view

--- lib/utils/app_theme/app_theme.dart
+++ lib/utils/app_theme/app_theme.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 class AppTheme{
   static final  ThemeData dark = ThemeData(
-    useMaterial3: false,
     scaffoldBackgroundColor: ColorsManager.black,
   );
 }
