420c6c06405757bbed4830b355956ab4d66c3282
ahmed <ahmedmohie553@gmail.com>
2025-03-03 19:12:51 +0200

    home repo bookModel

---
+++ lib/features/presentation/home/data/repos/home_repo.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+import '../models/book_model.dart';
+
+abstract class HomeRepo
+{
+ Future<Either< Failure ,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either< Failure ,List<BookModel>>> fetchFeaturedBooks();
+}
+
