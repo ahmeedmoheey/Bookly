f63b13117f0ebec43cf268c91a6f172917147646
ahmed <ahmedmohie553@gmail.com>
2025-03-06 16:25:40 +0200

    logo

--- lib/core/images_manager.dart
+++ lib/core/images_manager.dart
@@ -1,4 +1,4 @@
 class AssetsManager{
-  static const String booklyLogo = 'assets/images/Logo.png';
+  static const String booklyLogo = 'assets/images/logo.png';
   static const String testImage = 'assets/images/test_image.png';
 }
